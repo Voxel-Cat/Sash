d48e7ca186ee4ebc569e4150117fddb7712169aa
miles <milestrex2011@icloud.com>
2026-09-14 14:35:42 -0700

    sashscreen

---
+++ project/sashscreen.gd
@@ -0,0 +1,13 @@
+extends Node2D
+# 1. Reference the scene (preloading is best for performance)
+const badgeScene = preload("res://SashBadge.tscn")
+
+func spawn_bullet():
+	# 2. Create an instance of the scene in memory
+	var badge = badgeScene.instantiate()
+	
+	# Optional: Modify properties before adding it to the tree
+	badge.position = Vector2(100, 200)
+	
+	# 3. Add it as a child of the current node to make it appear in the game
+	add_child(badge)
